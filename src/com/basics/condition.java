b9cb6bc2ec10f2955ec94d3019139328ece165b1
Tharunyasakthi <tharusakthi3@gmail.com>
2018-05-30 03:51:55 -0700

    condition

---
+++ src/com/basics/condition.java
@@ -0,0 +1,19 @@
+package com.basics;
+
+public class condition {
+	public static void main(String args[]) {
+		int i,j,k;
+	
+for (i= 1; i <= 5; i++ )
+{
+for (j= 1; j <= 3; j++ )
+{
+for (k= 1; k <= 4; k++ )
+System.out.print( '*' );
+System.out.println();
+}
+System.out.println();
+} 
+
+}
+}
