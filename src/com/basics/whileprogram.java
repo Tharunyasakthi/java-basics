fe4e69bc5eab8c18382dd24795ffe67767937f47
Tharunyasakthi <tharusakthi3@gmail.com>
2018-05-30 00:08:09 -0700

    whilesample

---
+++ src/com/basics/whileprogram.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+public class whileprogram {
+	public static void main(String args[]) 
+	{
+		int i=0;
+		while(i<10) 
+		{
+			System.out.println("i="+i);
+			i++;
+	}
+
+}
+}
