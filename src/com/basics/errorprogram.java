e9d3564df44cdacbc4291eb347eb957c68d6827a
Tharunyasakthi <tharusakthi3@gmail.com>
2018-05-30 00:19:17 -0700

    errorprogram1

---
+++ src/com/basics/errorprogram.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+public class errorprogram {
+	public static void main(String args[])
+	{
+		int age=100;
+            if(age >= 65 )
+            {
+            System.out.println( "Age is greater than or equal to 65" );
+            }
+            else
+            System.out.println( "Age is less than 65");
+}
+}
