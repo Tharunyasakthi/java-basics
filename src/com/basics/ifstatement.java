eca1f42f983eca88fa0e6894506b5531f714291d
Tharunyasakthi <tharusakthi3@gmail.com>
2018-05-29 22:13:03 -0700

    ifsample

---
+++ src/com/basics/ifstatement.java
@@ -0,0 +1,15 @@
+package com.basics;
+
+public class ifstatement {
+	public static void main(String args[])
+	{
+		int i=150;
+		if (i>50)
+		{
+			System.out.println("win");
+		}
+		else
+			System.out.println("loss");
+	}
+
+}
