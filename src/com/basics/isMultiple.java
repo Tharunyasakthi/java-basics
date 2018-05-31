eb80284503a030e6872f92c508c0fd700d24f9cd
Tharunyasakthi <tharusakthi3@gmail.com>
2018-05-31 05:34:58 -0700

    isMultiple

---
+++ src/com/basics/isMultiple.java
@@ -0,0 +1,16 @@
+package com.basics;
+
+	public class isMultiple {
+		public static void main(String v[])
+		{
+			isMultiple t1=new isMultiple();
+		    System.out.print(t1.isMultiple1(2,3));
+		}
+	public boolean  isMultiple1 (int one,int two)
+	{
+		return two%one==0?true:false;
+	}
+	}
+
+
+
