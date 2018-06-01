e7274c42d92a6e2b9083014941e6bd0600f6e38b
Tharunyasakthi <tharusakthi3@gmail.com>
2018-06-01 05:51:47 -0700

    inheritance1

---
+++ src/com/basics/taxi.java
@@ -0,0 +1,14 @@
+package com.basics;
+
+public class taxi{
+	
+		public taxi() {
+			System.out.println("empty constructor");
+		}
+		public static void main(String args[]) {
+			taxi t1 =new taxi();
+			
+}
+
+}
+                                            
