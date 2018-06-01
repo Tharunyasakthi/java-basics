bdf374422694375306d63207239e4d77c46cf350
Tharunyasakthi <tharusakthi3@gmail.com>
2018-06-01 05:50:50 -0700

    inheritance

---
+++ src/com/basics/EmptyCar.java
@@ -0,0 +1,12 @@
+package com.basics;            
+
+public class EmptyCar extends taxi {
+public EmptyCar()
+{
+	System.out.println("new constructor");
+}
+public static void main(String args[]) {
+	EmptyCar c1 = new EmptyCar();
+}
+	
+}
