cd6c287e89ce282d009b2d22b180cd28fa3ae71d
Tharunyasakthi <tharusakthi3@gmail.com>
2018-06-04 00:03:48 -0700

    byteconversion

---
+++ src/com/basics/TypeCast.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class TypeCast {
+
+	public static void main(String args[]) {
+		byte one=3;
+		byte two=4;
+	int three=one+two;
+		
+		System.out.println(three);
+	}
+}
