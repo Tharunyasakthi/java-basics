93967aaf8e47e6fce9bc313312ba63088a8045ee
Tharunyasakthi <tharusakthi3@gmail.com>
2018-06-03 23:37:36 -0700

    primitiveprogram

---
+++ src/com/basics/Primitive.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class Primitive {
+	public static void main(String args[]) {
+	int number=4; 
+	String fan;
+	double speed=4.4;
+	 
+	System.out.println("speed"+speed);
+}
+	}
