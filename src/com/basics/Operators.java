8a4189bc5fcbb48868882dc659bb2f67bfa26a33
Tharunyasakthi <tharusakthi3@gmail.com>
2018-06-04 02:09:14 -0700

    operatorsprogram

---
+++ src/com/basics/Operators.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class Operators {
+	public static void main(String args[]) {
+      int sign = 20;
+      sign++;
+      sign--;
+      System.out.println(sign==200);
+      System.out.println(sign>=200);
+}
+}
