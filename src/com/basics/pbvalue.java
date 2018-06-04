88dd0949a5dd4322838a21f6b2340d10b2c2924e
Tharunyasakthi <tharusakthi3@gmail.com>
2018-06-04 04:38:41 -0700

    pbvalue

---
+++ src/com/basics/pbvalue.java
@@ -0,0 +1,26 @@
+package com.basics;
+
+public class pbvalue {
+public static void main(String[] args) {
+	 
+	 one obj=new one();
+	 int d = obj.c(10, 12);
+	 System.out.println(d);
+	
+	
+	 
+	 
+}
+   
+    
+
+
+}
+class one{
+    	public static int c(int a,int b) {
+    		int c=a+b;
+			return c;
+    		
+    	}
+ 	
+}
